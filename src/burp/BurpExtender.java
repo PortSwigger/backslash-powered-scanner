74add1fd96706f0a67e920db8e1379d7bce25f16
James Kettle <james.kettle@portswigger.net>
2025-03-25 12:37:33 +0000

    Re-enable scanning via context menu

--- src/burp/BurpExtender.java
+++ src/burp/BurpExtender.java
@@ -38,7 +38,7 @@ public class BurpExtender implements IBurpExtender, BurpExtension {
 
     public void initialize(MontoyaApi api) {
         Utilities.montoyaApi = api;
-
+        BulkUtilities.registerContextMenu();
         Utilities.montoyaApi.userInterface().registerContextMenuItemsProvider(new OfferWsFuzz(callbacks));
     }
 
